12c2d276535c68f4d5d011f78d9b5b8538765457
Fatima8370 <fatimamazhar2006@gmail.com>
2026-07-23 15:50:32 +0500

    Update issues.py

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,6 +1,6 @@
 import uuid
 from fastapi import APIRouter, HTTPException, status
-from app.schemas import IssueCreate, IssueOut, IssueUpdate
+from app.schemas import IssueCreate, IssueOut, IssueUpdate, IssueStatus
 from app.storage import load_data, save_data
 
 # class used to group related API routes
@@ -13,6 +13,18 @@ def get_issues():
     issues = load_data
     return issues
 
+@router.post("/", response_model= "IssueOut", status_code= status.HTTP_201_CREATED)
+def create_issue(issue: IssueCreate):
+    '''Create New issue'''
+    issues = load_data()
+    new_issue = IssueOut(
+        id= str (uuid.uuid4()),
+        title= issue.title,
+        description= issue.description,
+        priority= issue.priority,
+            status= IssueStatus.open,
+    )
+    
 
 
 
